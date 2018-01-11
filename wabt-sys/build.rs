ab29827bac587ec25dd66b22d48128351043fe8d
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-01-11 23:40:34 +0300

    initial impl of sys crate

--- wabt-sys/build.rs
+++ wabt-sys/build.rs
@@ -2,14 +2,34 @@ extern crate bindgen;
 extern crate cmake;
 extern crate cc;
 
+use std::env;
+
 fn main() {
     let dst = cmake::Config::new("wabt")
         .define("BUILD_TESTS", "OFF")
         .build();
     println!("cargo:rustc-link-search=native={}/build/", dst.display());
     println!("cargo:rustc-link-lib=static=wabt");
+    
+    // We need to link against C++ std lib
+    if let Some(cpp_stdlib) = get_cpp_stdlib() {
+        println!("cargo:rustc-link-lib={}", cpp_stdlib);
+    }
+}
 
-    cc::Build::new()
-        .file("wabt/src/emscripten-helpers.cc")
-        .compile("emscripten");
+// See https://github.com/alexcrichton/gcc-rs/blob/88ac58e25/src/lib.rs#L1197
+fn get_cpp_stdlib() -> Option<String> {
+    env::var("TARGET").ok().and_then(|target| {
+        if target.contains("msvc") {
+            None
+        } else if target.contains("darwin") {
+            Some("c++".to_string())
+        } else if target.contains("freebsd") {
+            Some("c++".to_string())
+        } else if target.contains("musl") {
+            Some("static=stdc++".to_string())
+        } else {
+            Some("stdc++".to_string())
+        }
+    })
 }
--- wabt-sys/src/lib.rs
+++ wabt-sys/src/lib.rs
@@ -1,7 +1,123 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::os::raw::{c_char, c_int, c_void};
+
+pub enum WastLexer {}
+pub enum ErrorHandlerBuffer {}
+pub enum WabtParseWatResult {}
+pub enum WasmModule {}
+pub enum WabtWriteModuleResult {}
+pub enum OutputBuffer {}
+
+#[derive(PartialEq, Eq)]
+#[repr(C)]
+pub enum ResultEnum {
+    Ok,
+    Error,
+}
+
+extern "C" {
+    pub fn wabt_new_wast_buffer_lexer(
+        filename: *const c_char,
+        data: *const c_void,
+        size: usize,
+    ) -> *mut WastLexer;
+
+    pub fn wabt_destroy_wast_lexer(lexer: *mut WastLexer);
+
+    pub fn wabt_new_text_error_handler_buffer() -> *mut ErrorHandlerBuffer;
+
+    pub fn wabt_new_binary_error_handler_buffer() -> *mut ErrorHandlerBuffer;
+
+    pub fn wabt_destroy_error_handler_buffer(error_handler: *mut ErrorHandlerBuffer);
+
+    pub fn wabt_parse_wat(
+        lexer: *mut WastLexer,
+        error_handler: *mut ErrorHandlerBuffer,
+    ) -> *mut WabtParseWatResult;
+
+    pub fn wabt_parse_wat_result_get_result(result: *mut WabtParseWatResult) -> ResultEnum;
+
+    pub fn wabt_parse_wat_result_release_module(result: *mut WabtParseWatResult)
+        -> *mut WasmModule;
+
+    pub fn wabt_destroy_parse_wat_result(result: *mut WabtParseWatResult);
+
+    pub fn wabt_resolve_names_module(
+        lexer: *mut WastLexer,
+        module: *mut WasmModule,
+        error_handler: *mut ErrorHandlerBuffer,
+    ) -> ResultEnum;
+
+    pub fn wabt_validate_module(
+        lexer: *mut WastLexer,
+        module: *mut WasmModule,
+        error_handler: *mut ErrorHandlerBuffer,
+    ) -> ResultEnum;
+
+    pub fn wabt_write_binary_module(
+        module: *mut WasmModule,
+        log: c_int,
+        canonicalize_lebs: c_int,
+        relocatable: c_int,
+        write_debug_name: c_int,
+    ) -> *mut WabtWriteModuleResult;
+
+    pub fn wabt_write_module_result_get_result(result: *mut WabtWriteModuleResult) -> ResultEnum;
+
+    pub fn wabt_write_module_result_release_output_buffer(
+        result: *mut WabtWriteModuleResult,
+    ) -> *mut OutputBuffer;
+
+    pub fn wabt_output_buffer_get_data(buffer: *mut OutputBuffer) -> *const c_void;
+
+    pub fn wabt_output_buffer_get_size(buffer: *mut OutputBuffer) -> usize;
+
+    pub fn wabt_destroy_output_buffer(buffer: *mut OutputBuffer);
+}
+
+#[test]
+fn create_destroy_lexer() {
+    use std::ffi::CString;
+    use std::slice;
+
+    let filename = CString::new("test.wast").unwrap();
+    let data = CString::new("(module)").unwrap();
+
+    unsafe {
+        let error_handler = wabt_new_text_error_handler_buffer();
+        let lexer =
+            wabt_new_wast_buffer_lexer(filename.as_ptr(), data.as_ptr() as *const c_void, 8);
+
+        let result = wabt_parse_wat(lexer, error_handler);
+        assert!(wabt_parse_wat_result_get_result(result) == ResultEnum::Ok);
+
+        let module = wabt_parse_wat_result_release_module(result);
+
+        let result = wabt_resolve_names_module(lexer, module, error_handler);
+        assert!(result == ResultEnum::Ok);
+
+        let result = wabt_validate_module(lexer, module, error_handler);
+        assert!(result == ResultEnum::Ok);
+
+        let result = wabt_write_binary_module(module, 0, 1, 0, 0);
+        assert!(wabt_write_module_result_get_result(result) == ResultEnum::Ok);
+
+        let output_buffer = wabt_write_module_result_release_output_buffer(result);
+
+        let data = slice::from_raw_parts(
+            wabt_output_buffer_get_data(output_buffer) as *const u8,
+            wabt_output_buffer_get_size(output_buffer),
+        );
+
+        assert_eq!(
+            data,
+            &[
+                0, 97, 115, 109, // \0ASM - magic
+                1, 0, 0, 0       //    01 - version
+            ]
+        );
+
+        wabt_destroy_output_buffer(output_buffer);
+        wabt_destroy_wast_lexer(lexer);
+        wabt_destroy_error_handler_buffer(error_handler);
     }
 }
