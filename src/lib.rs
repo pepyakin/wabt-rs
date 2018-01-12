9d6e203c73e4d964a6638901ab32522410a1b71f
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-01-12 18:48:23 +0300

    Add wasm2wat.

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,43 @@
 //! Bindings to the [wabt](https://github.com/WebAssembly/wabt) library.
-//! 
+//!
 
 extern crate wabt_sys;
 
-use std::os::raw::c_void;
+use std::os::raw::{c_void, c_int};
 use std::ffi::CString;
 use std::ptr;
 
 use wabt_sys::*;
 
 /// A structure to represent errors coming out from wabt.
-/// 
+///
 /// Actual errors are not yet published.
 #[derive(Debug, PartialEq, Eq)]
 pub struct Error(ErrorKind);
 
 #[derive(Debug, PartialEq, Eq)]
 enum ErrorKind {
+    Deserialize,
     Parse,
+    WriteText,
     ResolveNames,
     Validate,
 }
 
 /// Translate wasm text source to wasm binary format.
-/// 
-/// If wasm source is valid wasm binary will be returned in the vector. 
+///
+/// If wasm source is valid wasm binary will be returned in the vector.
 /// Returned binary is validated and can be executed.
-/// 
+///
 /// For more examples and online demo you can check online version
 /// of [wat2wasm](https://cdn.rawgit.com/WebAssembly/wabt/aae5a4b7/demo/wat2wasm/).
-/// 
+///
 /// # Examples
-/// 
+///
 /// ```rust
 /// extern crate wabt;
 /// use wabt::wat2wasm;
-/// 
+///
 /// fn main() {
 ///     assert_eq!(
 ///         wat2wasm("(module)").unwrap(),
@@ -46,21 +48,23 @@ enum ErrorKind {
 ///     );
 /// }
 /// ```
-/// 
+///
 pub fn wat2wasm(src: &str) -> Result<Vec<u8>, Error> {
     let filename = CString::new("test.wast").unwrap();
     let data = CString::new(src).unwrap();
 
     unsafe {
         let error_handler = wabt_new_text_error_handler_buffer();
-        let lexer =
-            wabt_new_wast_buffer_lexer(filename.as_ptr(), data.as_ptr() as *const c_void, src.len());
+        let lexer = wabt_new_wast_buffer_lexer(
+            filename.as_ptr(),
+            data.as_ptr() as *const c_void,
+            src.len(),
+        );
 
         let result = wabt_parse_wat(lexer, error_handler);
         if wabt_parse_wat_result_get_result(result) == ResultEnum::Error {
             return Err(Error(ErrorKind::Parse));
         }
-
         let module = wabt_parse_wat_result_release_module(result);
 
         let result = wabt_resolve_names_module(lexer, module, error_handler);
@@ -94,6 +98,53 @@ pub fn wat2wasm(src: &str) -> Result<Vec<u8>, Error> {
     }
 }
 
+/// Disassemble wasm binary to wasm text format.
+///
+/// # Examples
+///
+/// ```rust
+/// extern crate wabt;
+/// use wabt::wasm2wat;
+///
+/// fn main() {
+///     assert_eq!(
+///         wasm2wat(&[
+///             0, 97, 115, 109, // \0ASM - magic
+///             1, 0, 0, 0       //    01 - version
+///         ]),
+///         Ok("(module)\n".to_owned()),
+///     );
+/// }
+/// ```
+///
+pub fn wasm2wat(wasm: &[u8]) -> Result<String, Error> {
+    unsafe {
+        let error_handler = wabt_new_binary_error_handler_buffer();
+        let result = wabt_read_binary(wasm.as_ptr(), wasm.len(), true as c_int, error_handler);
+        if wabt_read_binary_result_get_result(result) == ResultEnum::Error {
+            return Err(Error(ErrorKind::Deserialize));
+        }
+        let module = wabt_read_binary_result_release_module(result);
+        wabt_destroy_read_binary_result(result);
+
+        let result = wabt_write_text_module(module, 0, 0);
+        if wabt_write_module_result_get_result(result) == ResultEnum::Error {
+            return Err(Error(ErrorKind::WriteText));
+        }
+        let output_buffer = wabt_write_module_result_release_output_buffer(result);
+
+        let data = wabt_output_buffer_get_data(output_buffer);
+        let size = wabt_output_buffer_get_size(output_buffer);
+
+        let mut buf: Vec<u8> = Vec::with_capacity(size);
+        ptr::copy_nonoverlapping(data as *const u8, buf.as_mut_ptr(), size);
+        buf.set_len(size);
+
+        let text = String::from_utf8(buf).unwrap();
+        Ok(text)
+    }
+}
+
 #[test]
 fn test_wat2wasm() {
     assert_eq!(
@@ -105,12 +156,45 @@ fn test_wat2wasm() {
         wat2wasm(
             r#"
             (module
-            )"#).unwrap(),
+            )"#,
+        ).unwrap(),
         &[0, 97, 115, 109, 1, 0, 0, 0]
     );
 
+    assert_eq!(wat2wasm("(modu"), Err(Error(ErrorKind::Parse)));
+}
+
+#[test]
+fn test_wasm2wat() {
     assert_eq!(
-        wat2wasm("(modu"),
-        Err(Error(ErrorKind::Parse))
+        wasm2wat(&[
+            0, 97, 115, 109, // \0ASM - magic
+            1, 0, 0, 0       //    01 - version
+        ]),
+        Ok("(module)\n".to_owned()),
     );
+
+    assert_eq!(
+        wasm2wat(&[
+            0, 97, 115, 109, // \0ASM - magic
+        ]),
+        Err(Error(ErrorKind::Deserialize)),
+    );
+}
+
+#[test]
+#[cfg_attr(rustfmt, rustfmt_skip)]
+fn roundtrip() {
+    #[cfg_attr(rustfmt, rustfmt_skip)]
+    let factorial = &[
+        0, 97, 115, 109, 1, 0, 0, 0, 1, 6, 1, 96, 1, 124, 1, 124, 3, 2, 1, 0, 7, 7, 
+        1, 3, 102, 97, 99, 0, 0, 10, 46, 1, 44, 0, 32, 0, 68, 0, 0, 0, 0, 0, 0, 240, 
+        63, 99, 4, 124, 68, 0, 0, 0, 0, 0, 0, 240, 63, 5, 32, 0, 32, 0, 68, 0, 0, 0, 
+        0, 0, 0, 240, 63, 161, 16, 0, 162, 11, 11
+    ];
+
+    let text = wasm2wat(factorial).unwrap();
+    let binary = wat2wasm(&text).unwrap();
+
+    assert_eq!(&*factorial as &[u8], &*binary);
 }
--- wabt-sys/src/lib.rs
+++ wabt-sys/src/lib.rs
@@ -5,9 +5,10 @@ pub enum ErrorHandlerBuffer {}
 pub enum WabtParseWatResult {}
 pub enum WasmModule {}
 pub enum WabtWriteModuleResult {}
+pub enum WabtReadBinaryResult {}
 pub enum OutputBuffer {}
 
-#[derive(PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 #[repr(C)]
 pub enum ResultEnum {
     Ok,
@@ -47,6 +48,14 @@ extern "C" {
         error_handler: *mut ErrorHandlerBuffer,
     ) -> ResultEnum;
 
+    pub fn wabt_apply_names_module(
+        module: *mut WasmModule,
+    ) -> ResultEnum;
+
+    pub fn wabt_generate_names_module(
+        module: *mut WasmModule,
+    ) -> ResultEnum;
+
     pub fn wabt_validate_module(
         lexer: *mut WastLexer,
         module: *mut WasmModule,
@@ -76,52 +85,67 @@ extern "C" {
     pub fn wabt_output_buffer_get_size(buffer: *mut OutputBuffer) -> usize;
 
     pub fn wabt_destroy_output_buffer(buffer: *mut OutputBuffer);
+
+    pub fn wabt_read_binary(
+        data: *const u8,
+        size: usize,
+        read_debug_names: c_int,
+        error_handler: *mut ErrorHandlerBuffer,
+    ) -> *mut WabtReadBinaryResult;
+
+    pub fn wabt_read_binary_result_get_result(
+        result: *mut WabtReadBinaryResult,
+    ) -> ResultEnum;
+
+    pub fn wabt_read_binary_result_release_module(
+        result: *mut WabtReadBinaryResult,
+    ) -> *mut WasmModule;
+
+    pub fn wabt_destroy_read_binary_result(
+        result: *mut WabtReadBinaryResult,
+    );
+
+    pub fn wabt_write_text_module(
+        module: *mut WasmModule,
+        fold_exprs: c_int,
+        inline_export: c_int,
+    ) -> *mut WabtWriteModuleResult;
 }
 
 #[test]
-fn create_destroy_lexer() {
-    use std::ffi::CString;
-    use std::slice;
+fn parse_wasm() {
+    use std::ptr;
 
-    let filename = CString::new("test.wast").unwrap();
-    let data = CString::new("(module)").unwrap();
+    let data: &[u8] = &[
+        0, 97, 115, 109, // \0ASM - magic
+        1, 0, 0, 0       //    01 - version
+    ];
 
     unsafe {
-        let error_handler = wabt_new_text_error_handler_buffer();
-        let lexer =
-            wabt_new_wast_buffer_lexer(filename.as_ptr(), data.as_ptr() as *const c_void, 8);
+        let error_handler = wabt_new_binary_error_handler_buffer();
+        let result = wabt_read_binary(
+            data.as_ptr(),
+            data.len(),
+            true as c_int,
+            error_handler,
+        );
+        assert_eq!(wabt_read_binary_result_get_result(result), ResultEnum::Ok);
+        let module = wabt_read_binary_result_release_module(result);
+        
+        wabt_destroy_read_binary_result(result);
 
-        let result = wabt_parse_wat(lexer, error_handler);
-        assert!(wabt_parse_wat_result_get_result(result) == ResultEnum::Ok);
-
-        let module = wabt_parse_wat_result_release_module(result);
-
-        let result = wabt_resolve_names_module(lexer, module, error_handler);
-        assert!(result == ResultEnum::Ok);
-
-        let result = wabt_validate_module(lexer, module, error_handler);
-        assert!(result == ResultEnum::Ok);
-
-        let result = wabt_write_binary_module(module, 0, 1, 0, 0);
-        assert!(wabt_write_module_result_get_result(result) == ResultEnum::Ok);
-
+        let result = wabt_write_text_module(module, 0, 0);
+        assert_eq!(wabt_write_module_result_get_result(result), ResultEnum::Ok);
         let output_buffer = wabt_write_module_result_release_output_buffer(result);
 
-        let data = slice::from_raw_parts(
-            wabt_output_buffer_get_data(output_buffer) as *const u8,
-            wabt_output_buffer_get_size(output_buffer),
-        );
+        let data = wabt_output_buffer_get_data(output_buffer);
+        let size = wabt_output_buffer_get_size(output_buffer);
 
-        assert_eq!(
-            data,
-            &[
-                0, 97, 115, 109, // \0ASM - magic
-                1, 0, 0, 0       //    01 - version
-            ]
-        );
+        let mut buf: Vec<u8> = Vec::with_capacity(size);
+        ptr::copy_nonoverlapping(data as *const u8, buf.as_mut_ptr(), size);
+        buf.set_len(size);
 
-        wabt_destroy_output_buffer(output_buffer);
-        wabt_destroy_wast_lexer(lexer);
-        wabt_destroy_error_handler_buffer(error_handler);
+        let text = String::from_utf8(buf).unwrap();
+        assert_eq!(&text, "(module)\n");
     }
 }
